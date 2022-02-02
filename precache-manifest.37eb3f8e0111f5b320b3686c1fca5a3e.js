1f50ba439f7748cb919181c93ff5f70d95316638
kunalahuja06 <imkunalahuja@gmail.com>
2022-02-02 16:21:57 +0530

    Updates

---
+++ precache-manifest.37eb3f8e0111f5b320b3686c1fca5a3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61698ccbc3b7fc9cb2101389073b4efb",
+    "url": "/Stripe-Clone/index.html"
+  },
+  {
+    "revision": "e84a8a62d80f703810d6",
+    "url": "/Stripe-Clone/static/css/main.3ae4daef.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/Stripe-Clone/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stripe-Clone/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84a8a62d80f703810d6",
+    "url": "/Stripe-Clone/static/js/main.f7508cbb.chunk.js"
+  },
+  {
+    "revision": "020d4c6b7aabfbaf05da",
+    "url": "/Stripe-Clone/static/js/runtime-main.a4da6767.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/Stripe-Clone/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/Stripe-Clone/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/Stripe-Clone/static/media/phone.e908706c.svg"
+  }
+]);
